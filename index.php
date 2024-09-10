d775bf7d945eea64467f2ce5b25d01765b06849a
evgeniy.gert <evgeniy.gert@csd.ru>
2024-09-10 23:33:48 +0300

    code formatting

--- index.php
+++ index.php
@@ -12,14 +12,14 @@
 */
 
 $tree = [
-    [ 'id' => '8', 'parent_id' => '6', ],
-    [ 'id' => '2', 'parent_id' => '1', ],
-    [ 'id' => '3', 'parent_id' => '1', ],
-    [ 'id' => '4', 'parent_id' => '1', ],
-    [ 'id' => '5', 'parent_id' => '2', ],
-    [ 'id' => '1', 'parent_id' => '0', ],
-    [ 'id' => '6', 'parent_id' => '2', ],
-    [ 'id' => '7', 'parent_id' => '3', ],
+    ['id' => '8', 'parent_id' => '6',],
+    ['id' => '2', 'parent_id' => '1',],
+    ['id' => '3', 'parent_id' => '1',],
+    ['id' => '4', 'parent_id' => '1',],
+    ['id' => '5', 'parent_id' => '2',],
+    ['id' => '1', 'parent_id' => '0',],
+    ['id' => '6', 'parent_id' => '2',],
+    ['id' => '7', 'parent_id' => '3',],
 ];
 PrintTree($tree);
 
@@ -27,7 +27,7 @@ function PrintTree($tree): void
 {
     $result = [0];
     while (!empty($result)) {
-        $root =array_pop($result);
+        $root = array_pop($result);
         $i = 0;
         while (isset($tree[$i])) {
             if ($tree[$i]['parent_id'] == $root) {
@@ -35,7 +35,7 @@ function PrintTree($tree): void
                     $result[] = $root;
                 }
                 $result[] = $tree[$i]['id'];
-                $str =  implode('.',$result);
+                $str = implode('.', $result);
                 echo "$str\n";
                 array_splice($tree, $i, 1);
                 break;
